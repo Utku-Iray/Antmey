0397525fbde51eb4d41b8e64d0eee4aa956dd801
Utku-Iray <51332027+Utku-Iray@users.noreply.github.com>
2023-08-02 11:39:31 +0300

    rev

--- php/head.php
+++ php/head.php
@@ -2,14 +2,14 @@
 <html lang="en">
 <head>
 <meta charset="utf-8">
-<title>ANTMEY </title>
+<title>Antmey | Antalya Meyveli İçecekler</title>
 <!-- Stylesheets -->
 <link href="css/bootstrap.css" rel="stylesheet">
 <link href="css/style.css" rel="stylesheet">
 <link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
 <link rel="icon" type="image/x-icon" href="images/favicon.png" >
 <!-- Responsive -->
-<meta name="description" content="2006 yılında kurulan tesisimiz 2022 yılında Antalya Meyveli İçecekler bünyesine katılmıştır. Üretimine Antalya Organize Sanayi Bölgesi karşısı Toptancılar Birliği Sitesinde devam etmektedir. Fabrikamızda kendi tescilli markalarımızla yaptığımız ürün grupları: Meyveli Konsantre ( 1+20 ) şuruplar, Karlama ( Ice Slush ), Kokteyl Şurupları ( 1+8 ), Meyve Püreleri, Topping Dekor Sosları, El yapımı limonata bazı ( 1+4 ), Sıkma Portakal Suyu. Gazlı İçecek Grubu: Üzüm Aromalı Gazlı İçecek, Tonik, Bitter Limon, Enerji İçeceği. Gıda Tarım ve Orman Bakanlığı üretim izniyle, İyi Üretim Uygulamaları ( GMP ) ile yapılan ürünlerimiz; müşteri beklentileri doğrultusunda , sorunsuz hizmet anlayışı ile Akdeniz merkez olmak üzere Ege, Marmara, İç Anadolu Bölgeleri ve Kıbrıs’da EDT ve Market kanalına sunulmaktadır.">
+<meta name="description" content="Tüketicileri kaliteli ve lezzetli içecekler ile buluşturmak öncelikli misyonumuzdur. Ürünlerimiz için bizlere iletişim kısmından ulaşabilirsiniz..">
 <meta name="keywords" content="Antmey, Antmey İçecek, Kokteyl Şurupları, Kokteyl, Şuruplar, Captain Custo Alkolsüz Şampanya, Captain Custo Kokteyl Şurupları, Boje Moy Kokteyl Şurupları,Meymiks Konsantre İçecekler (Meyveli Şuruplar), Taze Meyve Suları, Captain Custo Püre, Captain Custo Topping Dekor Sosları, Gazlı İçecekler">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">

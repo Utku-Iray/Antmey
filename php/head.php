cf18953d5d7fff46b9f9dad07feaf5be823034a7
Utku-Iray <51332027+Utku-Iray@users.noreply.github.com>
2024-01-26 16:53:33 +0300

    re

--- php/head.php
+++ php/head.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
 <meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
 <meta name="description" content="Tüketicileri kaliteli ve lezzetli içecekler ile buluşturmak öncelikli misyonumuzdur. Ürünlerimiz için bizlere iletişim kısmından ulaşabilirsiniz..">
 <title>Antmey | Antalya Meyveli İçecekler</title>
 <!-- Stylesheets -->
@@ -12,7 +13,6 @@
 <!-- Responsive -->
 <meta name="keywords" content="Antmey, Antmey İçecek, Kokteyl Şurupları, Kokteyl, Şuruplar, Captain Custo Alkolsüz Şampanya, Captain Custo Kokteyl Şurupları, Boje Moy Kokteyl Şurupları,Meymiks Konsantre İçecekler (Meyveli Şuruplar), Taze Meyve Suları, Captain Custo Püre, Captain Custo Topping Dekor Sosları, Gazlı İçecekler">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
 <link href="css/responsive.css" rel="stylesheet">
 <!--[if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script><![endif]--> 
 
